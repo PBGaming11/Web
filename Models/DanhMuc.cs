da13cab59ae2f0db7630f539b7bddd974b2da1ab
PBGaming11 <phanbaobeo789@gmail.com>
2024-07-17 15:08:51 +0700

    Add project files.

---
+++ Models/DanhMuc.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class DanhMuc
+    {
+        [Key] // khoá để chỉ đến nó tự động và động nhất
+        public int Id { get; set; }
+        [Required] //yêu cầu bắt buộc
+        public string? Namedanhmuc { get; set; }
+        public DateTime Description { get; set; } = DateTime.Now;
+    }
+}
